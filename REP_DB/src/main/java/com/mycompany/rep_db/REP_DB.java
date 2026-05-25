6f3c4c92015ce77d8e6d6865b082a86134ecba10
René Esquivel <er23012@ues.edu.sv>
2026-05-24 22:53:36 -0600

    Add files via upload

---
+++ REP_DB/src/main/java/com/mycompany/rep_db/REP_DB.java
@@ -0,0 +1,13 @@
+package com.mycompany.rep_db;
+
+public class REP_DB {
+
+    public static void main(String[] args) {
+
+        login ventana = new login();
+        ventana.setVisible(true);
+
+
+    }
+}
+
